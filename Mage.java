8e3f72d4fce324be99719a43ec90242136b281c0
yan04s <130055001+yan04s@users.noreply.github.com>
2023-12-24 21:54:13 +0800

    24 Dec 2023
    src
    
    Assignment II_D220137B_TAN BING YAN

---
+++ Mage.java
@@ -0,0 +1,66 @@
+public class Mage extends Character {
+    private String elementalAffinity;
+    private int healthLock;
+
+    protected Mage(String characterID, String characterName, int level, int health,String elementalAffinity){
+        super(characterID, characterName, level, health);
+        this.elementalAffinity=elementalAffinity;
+    }
+
+    public String getElementalAffinity() {
+        return elementalAffinity;
+    }
+    public int getHealthLock() {
+        return healthLock;
+    }
+    public void setElementalAffinity(String elementalAffinity) {
+        this.elementalAffinity = elementalAffinity;
+    }
+    public void castElementalSpell(String spellType){
+        if(super.getLevel()<5&&super.getLevel()>0){
+            System.out.println("Level "+super.getLevel()+" is not support to cast Elemental Spell!! \n[You should reach at least Level 5 to unlock Elemental Spell]");
+            this.healthLock=0;
+        }else if(super.getLevel()>=5){
+            if(spellType.equalsIgnoreCase("teleportation")){
+                System.out.println("Teleporting "+super.getCharacterName()+" out of danger!");
+                this.healthLock=1;
+            }else if(spellType.equalsIgnoreCase("illusion")){
+                System.out.println(super.getCharacterName()+" is creating illusions to confuse the enemy. ");
+                this.healthLock=1;
+            }else if(spellType.equalsIgnoreCase("detection")){
+                System.out.println(super.getCharacterName()+" is detecting the presence of magic nearby... ");
+                this.healthLock=0;
+            }//end if
+        };//end if
+    }//end of cast ELEMENTAL SPELL
+
+    @Override
+    public String toString() {
+        String info="";
+
+        if(!(getPlayers().isEmpty())){
+            for (Player player : getPlayers()) {
+                info+="\nPlayer ID is "+player.getPlayerID()+"\nPlayer Name is "+player.getPlayerName();
+            }
+        }
+
+        info +="\nCharacter ID: " + super.getCharacterID() + "\nCharacter Name: " + super.getCharacterName()+"\nCharacter Level: "+super.getLevel()+"\nCharacter Health: "+super.getHealth();//+"\nCharacter Skills: "+super.getSkills();
+
+        if(!(getSkills().isEmpty())){
+            info+="\nCharacter Skills: ";
+            int count=1;
+            for (Skill skill : getSkills()) {
+                if(getSkills().size()>1){
+                    info+="\n"+count+")Skill Name: "+skill.getSkillName()+"\tSkill Description: "+skill.getSkillDescription();
+                    count++;
+                }else{
+                    info+="\nSkill Name: "+skill.getSkillName()+"\nSkill Description: "+skill.getSkillDescription();
+                }//end if
+            }//end for loop
+        }//end if
+
+        info+="\nCurrent Elemental Affinity: "+getElementalAffinity();
+        return info;
+
+    }
+}
---
+++ Rogue.java
@@ -0,0 +1,82 @@
+public class Rogue extends Character {
+    private int stealthLevel;
+    private String daggerType;
+    private int healthLock;
+
+    protected Rogue(String characterID, String characterName, int level, int health,int stealthLevel,String daggerType){
+        super(characterID, characterName, level, health);
+        this.stealthLevel=stealthLevel;
+        this.daggerType=daggerType;
+    }
+
+    public int getStealthLevel() {
+        return stealthLevel;
+    }
+    public String getDaggerType() {
+        return daggerType;
+    }
+    public int getHealthLock() {
+        return healthLock;
+    }
+    public void setStealthLevel(int stealthLevel) {
+        this.stealthLevel = stealthLevel;
+        evade();
+    }
+    public void setDaggerType(String daggerType) {
+        this.daggerType = daggerType;
+    }
+    public void stab(Character target){
+        if(target.getHealthLock()==0){
+            if(target.getLevel()>=1&&target.getLevel()<=3){
+                target.setHealth(target.getHealth()-10);
+            }else if(target.getLevel()>=4&&target.getLevel()<=5){
+                target.setHealth(target.getHealth()-15);
+            }else if(target.getLevel()>5){
+                target.setHealth(target.getHealth()-20);
+            };//end if
+            System.out.println("!Attack Success!");
+        }else{
+            System.out.println("⌜\n["+target.getCharacterName()+" was in Protected State]\nAttack Failed!\n\t\t\t\t\t\t⌟");
+        };//end if
+    }
+    public void evade(){
+        if(getStealthLevel()<5&&getStealthLevel()>0){
+            System.out.println("Stealth Level: "+getStealthLevel()+"\n[You must reach at least stealth level 5 to evading attacks]");
+            this.healthLock=0;
+        }else if(getStealthLevel()>=5){
+            System.out.println("Stealth Level: "+getStealthLevel()+"\tCharacter Name: "+super.getCharacterName()+"\n[Successfully Evaded Incoming Attack]");
+            this.healthLock=1;
+        };//end if
+    }
+
+    @Override
+    public String toString() {
+        String info="";
+
+        if(!(getPlayers().isEmpty())){
+            for (Player player : getPlayers()) {
+                info+="\nPlayer ID is "+player.getPlayerID()+"\nPlayer Name is "+player.getPlayerName();
+            }
+        }
+
+        info +="\nCharacter ID: " + super.getCharacterID() + "\nCharacter Name: " + super.getCharacterName()+"\nCharacter Level: "+super.getLevel()+"\nCharacter Health: "+super.getHealth();//+"\nCharacter Skills: "+super.getSkills();
+
+        if(!(getSkills().isEmpty())){
+            info+="\nCharacter Skills: ";
+            int count=1;
+            for (Skill skill : getSkills()) {
+                if(getSkills().size()>=2){
+                    info+="\n"+count+"Skill Name: "+skill.getSkillName()+"\tSkill Description: "+skill.getSkillDescription();
+                    count++;
+                }else{
+                    info+="\nSkill Name: "+skill.getSkillName()+"\nSkill Description: "+skill.getSkillDescription();
+                }//end if
+            }//end for loop
+        }//end if
+
+        info+="\nCurrent Dagger Type: "+getDaggerType()+"\nCurrent Stealth Level: "+getStealthLevel();
+        return info;
+
+    }
+
+}
